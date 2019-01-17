2ac09a3f842eb50f17c576237bcfc12e5d343b50
Jenkins Automation Server <jenkins@localhost.localdomain>
2019-01-17 10:23:52 -0500

    fix permissions

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -3,4 +3,3 @@
 # check for whether we're clang or gcc
 # setup the right options depending on the environment variables
 # run the build
-
--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 pip install --user --upgrade meson
-
