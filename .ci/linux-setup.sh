a6507218e8dbec7c20c3932ad7fc741e268f9db1
Jenkins Automation Server <jenkins@localhost.localdomain>
2019-01-18 14:41:21 -0500

    Get newer version of python, which Meson depends on to be installed

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-python3.5 -m pip install --upgrade pip
-python3.5 -m pip install --upgrade setuptools
+python3.5 -m pip install --upgrade pip --user
+python3.5 -m pip install --upgrade setuptools --user
 python3.5 -m pip install --upgrade meson --user
