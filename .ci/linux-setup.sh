eb6d8a762f3240eaaac1e5a4daa17710ec1fcfd8
Jenkins Automation Server <jenkins@localhost.localdomain>
2019-01-21 09:50:13 -0500

    cleaning things up

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -2,5 +2,5 @@
 
 
 python3.5 -m pip install --upgrade pip --user
-python3.5 -m pip install --upgrade setuptools --user
+#python3.5 -m pip install --upgrade setuptools --user
 python3.5 -m pip install --upgrade meson --user
