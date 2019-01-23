913fa9e483027ae01161bd8d62b9af5762f5a595
Aaron Conole <aconole@redhat.com>
2019-01-23 15:32:56 -0500

    linux-build: clean it up
    Just some cleanup
    
    Signed-off-by: Aaron Conole <aconole@redhat.com>

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -4,20 +4,29 @@
 # setup the right options depending on the environment variables
 # run the build
 
-if [ "${CC}" == "clang" ]; then
-    if [ "${MAKE}" == "1" ]; then
-	make config T=x86_64-native-linuxapp-clang
+# Just used for the 'classic' configuration system (ie: make)
+set_conf() {
+    c="$1/.config"
+    shift
+
+    if grep -q "$1" "$c"; then
+        sed -i "s:^$1=.*$:$1=$2:g" $c
+    else
+        echo $1=$2 >> "$c"
     fi
-elif [ "${CC}" == "gcc" ]; then
-    if [ "${MAKE}" == "1" ]; then
-	make config T=x86_64-native-linuxapp-gcc
-    fi
-else
-	echo "compiler env variable not set"
-	exit 1
-fi
+}
+
 
 if [ "${MAKE}" == "1" ]; then
+    make config T=x86_64-native-linuxapp-${CC}
+    if [ "${SHARED}" == "1" ]; then
+        set_conf build CONFIG_RTE_BUILD_SHARED_LIB y
+    fi
+
+    if [ "${KERNEL}" == "1" ]; then
+        echo Unsupported kernel builds at the moment
+    fi
+
     make all
 else
     meson build
