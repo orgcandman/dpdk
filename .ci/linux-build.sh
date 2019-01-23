469f6ac008c4f495d004734ce968ad9da6d41c96
Aaron Conole <aconole@redhat.com>
2019-01-23 15:23:13 -0500

    ci: enable ninja and meson builds
    Ninja+Meson as well as the legacy 'gnumake' builds are supported via
    travis build.
    
    Signed-off-by: Aaron Conole <aconole@redhat.com>

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -4,15 +4,22 @@
 # setup the right options depending on the environment variables
 # run the build
 
-if [ "${CC}" == "clang" ]
-then
+if [ "${CC}" == "clang" ]; then
+    if [ "${MAKE}" == "1" ]; then
 	make config T=x86_64-native-linuxapp-clang
-elif [ "${CC}" == "gcc" ]
-then
+    fi
+elif [ "${CC}" == "gcc" ]; then
+    if [ "${MAKE}" == "1" ]; then
 	make config T=x86_64-native-linuxapp-gcc
+    fi
 else
 	echo "compiler env variable not set"
 	exit 1
 fi
 
-make all
+if [ "${MAKE}" == "1" ]; then
+    make all
+else
+    meson build
+    ninja -C build
+fi
