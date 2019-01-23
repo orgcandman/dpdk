5fe330841b6139435870d29cd33c4e715aeed821
Michael Santana <msantana@redhat.com>
2019-01-23 17:07:14 -0500

    Introduce travis builds for github repositories
    GitHub is a service used by developers to store repositories.  GitHub
    provides service integrations that allow 3rd party services to access
    developer repositories and perform actions.  One of these services is
    Travis-CI, a simple continuous integration platform.
    
    This is a simple initial implementation of a travis build for the DPDK
    project.  It doesn't require any changes from individual developers to
    enable, but will allow those developers who opt-in to GitHub and the
    travis service to get automatic builds for every push they make.
    
    Additionally, the travis service will send an email to the test-report
    list informing anyone interested in the automated build (including a
    result).
    
    Signed-off-by: Aaron Conole <aconole@redhat.com>
    Signed-off-by: Michael Santana <msantana@redhat.com>
    Signed-off-by: 0-day Robot <robot_email>

---
+++ .ci/linux-build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# check for whether we're clang or gcc
+# setup the right options depending on the environment variables
+# run the build
+
+# Just used for the 'classic' configuration system (ie: make)
+set_conf() {
+    c="$1/.config"
+    shift
+
+    if grep -q "$1" "$c"; then
+        sed -i "s:^$1=.*$:$1=$2:g" $c
+    else
+        echo $1=$2 >> "$c"
+    fi
+}
+
+
+if [ "${NINJABUILD}" == "1" ]; then
+    meson build
+    ninja -C build
+else
+    make config T=x86_64-native-linuxapp-${CC}
+    if [ "${SHARED}" == "1" ]; then
+        set_conf build CONFIG_RTE_BUILD_SHARED_LIB y
+    fi
+
+    if [ "${KERNEL}" == "1" ]; then
+        echo Unsupported kernel builds at the moment
+    fi
+
+    make all
+fi
---
+++ .ci/linux-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3.5 -m pip install --upgrade meson --user
