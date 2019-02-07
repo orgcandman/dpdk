d5dd162340fcb7b057e4cc4c17fa153bc48aa902
Michael Santana <msantana@redhat.com>
2019-02-07 17:01:14 -0500

    ci: Introduce travis builds for github repositories
    GitHub is a service used by developers to store repositories.  GitHub
    provides service integrations that allow 3rd party services to access
    developer repositories and perform actions.  One of these services is
    Travis-CI, a simple continuous integration platform.
    
    This is a simple initial implementation of a travis build for the DPDK
    project.  It doesn't require any changes from individual developers to
    enable, but will allow those developers who opt-in to GitHub and the
    travis service to get automatic builds for every push they make.
    
    Additionally, the travis service will send an email to the test-report
    list informing anyone interested in the automated build (including a
    result).
    
    Signed-off-by: Aaron Conole <aconole@redhat.com>
    Signed-off-by: Michael Santana <msantana@redhat.com>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Reviewed-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>

---
+++ .ci/linux-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+function on_error() {
+    FILES_TO_PRINT=( "build/meson-logs/testlog.txt" "build/.ninja_log" "build/meson-logs/meson-log.txt")
+
+    for pr_file in "${FILES_TO_PRINT[@]}"; do
+        if [ -e "$pr_file" ]; then
+            cat "$pr_file"
+        fi
+    done
+}
+trap on_error ERR
+
+RUN_TESTS="yes"
+
+if [ "${AARCH64}" == "1" ]; then
+    # convert the arch specifier
+    OPTS="${OPTS} -DRTE_ARCH_64=1 --cross-file config/arm/arm64_armv8_linuxapp_gcc"
+    RUN_TESTS="no"
+fi
+
+OPTS="$OPTS --default-library=$DEF_LIB"
+meson build --werror -Dexamples=all ${OPTS}
+ninja -C build
+
+if [ "$RUN_TESTS" == "yes" ]; then
+    ninja -C build test
+fi
