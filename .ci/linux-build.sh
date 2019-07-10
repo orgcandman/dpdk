ccba975bdfe851b4c8ec3f208451bb105317b76d
Aaron Conole <aconole@redhat.com>
2019-07-10 10:58:40 -0400

    ci: enable unit tests under travis-ci
    When building under Travis (or another linux CI service), enable running the
    fast-tests for selected builds.  Only the shared builds are enabled at this
    point, since they are the ones passing.  Builds that are statically linked
    still show some issues in some of the eal_flags tests.  Additionally,
    the command to invoke fast tests includes a timeout multiplier, since
    some CI environments don't have enough resources to complete the tests in
    the default 10s timeout period.
    
    Signed-off-by: Aaron Conole <aconole@redhat.com>

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -22,3 +22,12 @@ fi
 OPTS="$OPTS --default-library=$DEF_LIB"
 meson build --werror -Dexamples=all $OPTS
 ninja -C build
+
+if [ "$RUN_TESTS" = "1" ]; then
+    # On the test build, also build the documentation, since it's expensive
+    # and we shouldn't need to build so much of it.
+    ninja -C build doc
+
+    sudo ninja -C build install
+    sudo meson test -C build --suite fast-tests -t 3
+fi
